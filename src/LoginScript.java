607fe20e2b4798f120571a664d10032d8b46b225
Ryan <62509624+ryanmathewdsouza@users.noreply.github.com>
2022-05-22 17:10:12 +0100

    bringing it all together

--- src/LoginScript.java
+++ src/LoginScript.java
@@ -5,12 +5,8 @@ import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Scanner;
 
-public class User {
-    public String uname;
-    public static String pass;
-
-    public static User login() throws Exception {
-
+public class LoginScript{
+    public static boolean LScript() {
         var loginDetails = new Hashtable<String,String>(); // This is where the login details will sit
         try // Maximum Security Data Importer. Hopefully it's readable.
         {
@@ -22,44 +18,31 @@ public class User {
         {
             throw new RuntimeException(e);
         }
-
         int tryNum = 3;
         String uname;
         String pass;
-        // System.out.println(loginDetails) >> I think some of these don't work unless you copy them in
+        // System.out.println(loginDetails); I think some of the login details don't work, unless you copy them in.
         do {
             try {
                 Scanner input1 = new Scanner(System.in);
                 Scanner input2 = new Scanner(System.in);
                 System.out.println("Please enter your username: ");
-                uname = input1.next().toLowerCase();
+                uname = input1.next();
                 System.out.println("Now enter your password: ");
                 pass = input2.next();
                 if (pass.equals(loginDetails.get(uname))) {
                     System.out.println("Login successful!");
-
-                    User user = new User(uname, pass);
-                    return user;
-                    /*
-                    Here shall lie the code that gives access to the rest of the app somehow
-                    You know, like a proper login prompt would do it
-                    Probably some encryption protocol
-                     */
-
+                    tryNum = 0;
+                    return true;
                 } else throw new Exception("Invalid Credentials.");
-
             } catch (Exception e) {
                 tryNum--;
-                System.out.println(e);
+                System.out.println(e.toString());
                 if (tryNum == 0) {
                     System.out.println("Login Unsuccessful. Please contact your administrator!");
                 }
             }
         } while (tryNum > 0);
-        throw new Exception ("Maximum login attempts made.");
-    }
-    public User(String uname, String pass) {
-        this.uname = uname;
-        this.pass = pass;
+        return false;
     }
 }
