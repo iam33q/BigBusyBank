530723ba460201f9021a481cd73b0daa3717b0d6
iam33q <irisandrei97@gmail.com>
2022-05-23 22:41:46 +0100

    Incomplete Customer Class

--- src/TestStuff/testFileWriters.java
+++ src/TestStuff/testFileWriters.java
@@ -1,8 +1,29 @@
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class testFileWriters {
     @Test
-    void testReadCSVRecord(){
-
+    void testWriteCSVRecord(){
+        try {
+            CSVWriter writer = new CSVWriter(new FileWriter("table.csv"));
+            String[] niners = {"9","9","9","9","9","9","9","9"};
+            writer.writeNext(niners);
+            writer.close();
+            CSVReader reader = new CSVReader(new FileReader("table.csv"));
+            String[] maybeNiners = reader.readAll().toArray(String[][]::new)[0];
+            reader.close();
+            assertArrayEquals(niners,maybeNiners);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
