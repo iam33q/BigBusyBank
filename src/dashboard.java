1ee0c1a3cf4a673d7e0d38d20eb1c6468b2c20d2
Magda <magdagozdalik28@gmail.com>
2022-05-24 13:46:37 +0100

    Created BankAccount Class and CurrentAccount subclass

--- src/dashboard.java
+++ src/dashboard.java
@@ -1,6 +1,10 @@
 public class dashboard {
     public static void main(String[] args) throws Exception {
         User u = User.login();
-        System.out.println("hey there " + u.uname);
+        System.out.println("\nhey there " + u.uname);
+        //create current account
+        // Iris how to add that current account to clients database???
+        CurrentAccount.newCurrentAccount();
+
     }
 }
